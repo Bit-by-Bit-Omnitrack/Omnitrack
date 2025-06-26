ebf6f2613017aeca5c98dd43a509eed85ca38270
Molefe Tlotlo Molefe <tlotlomolefem@gmail.com>
2025-06-26 22:00:18 +0200

    Delete Omnitrack directory

--- Omnitrack/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Omnitrack.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
