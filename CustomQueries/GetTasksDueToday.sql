cb28aa2ab4b2542390a20de631860acdb02bce4b
Nkateko Mkhabela <99898616+NkatekoMkhebela@users.noreply.github.com>
2025-06-27 13:58:47 +0200

    Add files via upload
    Custom queties

---
+++ CustomQueries/GetTasksDueToday.sql
@@ -0,0 +1,11 @@
+﻿SELECT 
+    Id,
+    TaskName,
+    Description,
+    Due_Date,
+    CreatedByID,
+    CreatedOn,
+    ModifiedByID,
+    ModifiedOn
+FROM Tasks
+WHERE CAST(Due_Date AS DATE) = CAST(GETDATE() AS DATE);
