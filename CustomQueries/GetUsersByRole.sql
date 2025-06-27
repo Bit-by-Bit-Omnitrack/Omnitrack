bea3e79a195e383ce780581ffb4150c51038e797
Molefe Tlotlo Molefe <tlotlomolefem@gmail.com>
2025-06-27 16:08:15 +0200

    update

---
+++ CustomQueries/GetUsersByRole.sql
@@ -0,0 +1,15 @@
+﻿SELECT 
+    Id,
+    EmployeeNumber,
+    FirstName,
+    MiddleName,
+    LastName,
+    CONCAT(FirstName, ' ', MiddleName, ' ', LastName) AS Fullname,
+    PhoneNumber,
+    EmailAddress,
+    Country,
+    DateOfBirth,
+    Address,
+    Role
+FROM Users
+WHERE Role = 'Admin';
---
+++ Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserRoles.Data;
+using UserRoles.Models;
+using UserRoles.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+builder.Services.AddIdentity<Users, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 6;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// ?? Auto-create database on first run
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate(); // Applies pending migrations and creates DB if needed
+}
+
+await SeedService.SeedDatabase(app.Services); // Optional: Seed data
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
