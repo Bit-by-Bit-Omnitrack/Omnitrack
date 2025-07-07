ba0820f29bd113f0feb1e26d67f15011dec6cc0d
Tlotlo.Molefe <Tlotlo.Molefe@INSPD6KZ83T2>
2025-07-07 11:34:11 +0200

    updated

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -20,13 +20,13 @@ namespace UserRoles.Data
 
     public class AppDbContext : IdentityDbContext<Users>
 {
-        public DbSet<UserRoles.Models.Priority> Priorities { get; set; } = default!;
-    public DbSet<UserRoles.Models.TaskItem> TaskItems { get; set; } = default!;
+        
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) { }
-
-        public DbSet<UserRoles.Models.Checklists> Checklists { get; set; } = default!;
+    public DbSet<UserRoles.Models.Priority> Priorities { get; set; } = default!;
+    public DbSet<UserRoles.Models.TaskItem> TaskItems { get; set; } = default!;
+    public DbSet<UserRoles.Models.Checklists> Checklists { get; set; } = default!;
 
     public DbSet<UserRoles.Models.Comments> Comments { get; set; } = default!;
 
