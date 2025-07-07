e92ecd100f3bc17eacfbc7295b5d4d1083d611c6
Tshenolo Millicent Mogane <millicent9710@gmail.com>
2025-07-07 13:13:57 +0200

    Update ID on Ticket.cs

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -2,6 +2,7 @@
 {
     public class Ticket
     {
+        public int Id { get; set; }
         public int TicketID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
