f1ec5c1b67d989f762c7483ecaaf782a840acde8
Nkateko Mkhabela <99898616+NkatekoMkhebela@users.noreply.github.com>
2025-07-07 09:01:00 +0200

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,26 +1,42 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Data;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using UserRoles.Models;
-//using UserRoles.ViewModels;
+using UserRoles.ViewModels;
 
 namespace UserRoles.Data
 {
     public class AppDbContext : IdentityDbContext<Users>
     {
-        //public AppDbContext(DbContextOptions options) : base(options)
-
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // Changed constructor to use DbContextOptions<AppDbContext> to fix scaffolding and build error
+        public AppDbContext(DbContextOptions options) : base(options)
         {
         }
-        // public DbSet<UserRoles.Models.User> User { get; set; } = default!;
-        // public DbSet<RegisterViewModel> AspNetUsers { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<AppTask> AppTasks { get; set; }
-        public DbSet<Ticket> Tickets { get; set; }
-        public DbSet<Priority> Priorities { get; set; }
-        public DbSet<TaskItem> TaskItems { get; set; }
-        public DbSet<TicketAssignment> TicketAssignments { get; set; }
-        public DbSet<TicketStatus> TicketStatuses { get; set; }
-        public DbSet<ChecklistItem> ChecklistItems { get; set; }
+       
     }
+}
+
+
+
+    public class AppDbContext : IdentityDbContext<Users>
+{
+        public DbSet<UserRoles.Models.Priority> Priorities { get; set; } = default!;
+    public DbSet<UserRoles.Models.TaskItem> TaskItems { get; set; } = default!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserRoles.Models.Checklists> Checklists { get; set; } = default!;
+
+        public DbSet<UserRoles.Models.Comments> Comments { get; set; } = default!;
+    // public DbSet<UserRoles.Models.User> User { get; set; } = default!;
+    // public DbSet<RegisterViewModel> AspNetUsers { get; set; }
+    public DbSet<Role> Roles { get; set; } = default!;
+    public DbSet<AppTask> AppTasks { get; set; } = default!;
+    public DbSet<Ticket> Tickets { get; set; } = default!;
+    public DbSet<Priority> Priorities { get; set; } = default!;
+    public DbSet<TaskItem> TaskItems { get; set; } = default!;
+    public DbSet<TicketAssignment> TicketAssignments { get; set; } = default!;
+    public DbSet<TicketStatus> TicketStatuses { get; set; } = default!;
+    public DbSet<ChecklistItem> ChecklistItems { get; set; } = default!;
 }
