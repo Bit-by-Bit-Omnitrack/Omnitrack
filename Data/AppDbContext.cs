4ef374f238f58c537ce80fb7b0bd5cd52b466082
Nkateko Mkhabela <99898616+NkatekoMkhebela@users.noreply.github.com>
2025-07-07 10:58:17 +0200

    Update AppDbContext.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -26,16 +26,13 @@ namespace UserRoles.Data
     public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options) { }
 
-        public DbSet<UserRoles.Models.Checklists> Checklists { get; set; } = default!;
-
-        public DbSet<UserRoles.Models.Comments> Comments { get; set; } = default!;
+     
     // public DbSet<UserRoles.Models.User> User { get; set; } = default!;
     // public DbSet<RegisterViewModel> AspNetUsers { get; set; }
     public DbSet<Role> Roles { get; set; } = default!;
     public DbSet<AppTask> AppTasks { get; set; } = default!;
     public DbSet<Ticket> Tickets { get; set; } = default!;
-    public DbSet<Priority> Priorities { get; set; } = default!;
-    public DbSet<TaskItem> TaskItems { get; set; } = default!;
+   
     public DbSet<TicketAssignment> TicketAssignments { get; set; } = default!;
     public DbSet<TicketStatus> TicketStatuses { get; set; } = default!;
     public DbSet<ChecklistItem> ChecklistItems { get; set; } = default!;
