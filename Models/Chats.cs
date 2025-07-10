da57c0cbf26726565dafd6b98d381910972771a0
NkatekoTekkie <99898616+NkatekoMkhebela@users.noreply.github.com>
2025-07-10 16:13:46 +0200

    Enhanced chat UI

--- Models/Chats.cs
+++ Models/Chats.cs
@@ -1,13 +1,13 @@
-﻿namespace UserRoles.Models
-{
-    public class Chats
+﻿    namespace UserRoles.Models
     {
-            public int Id { get; set; }
-            public int TicketId { get; set; }
-            public string Sender { get; set; }
-            public string Message { get; set; }
-            public DateTime SentAt { get; set; }
+        public class Chats
+        {
+                public int Id { get; set; }
+                public int TicketId { get; set; }
+                public string Sender { get; set; }
+                public string Message { get; set; }
+                public DateTime SentAt { get; set; }
         
 
+        }
     }
-}
