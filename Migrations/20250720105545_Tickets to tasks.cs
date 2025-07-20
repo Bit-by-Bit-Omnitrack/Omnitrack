d53367065bb4441d8a4f8ba0ca2573ce956f3421
Molefe Tlotlo Molefe <tlotlomolefem@gmail.com>
2025-07-20 13:05:49 +0200

    being able to assign tasks to tickets

---
+++ Migrations/20250720105545_Tickets to tasks.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserRoles.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ticketstotasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Tasks_TasksId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TasksId",
+                table: "Tickets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Tasks_TasksId",
+                table: "Tickets",
+                column: "TasksId",
+                principalTable: "Tasks",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Tasks_TasksId",
+                table: "Tickets");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TasksId",
+                table: "Tickets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Tasks_TasksId",
+                table: "Tickets",
+                column: "TasksId",
+                principalTable: "Tasks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
