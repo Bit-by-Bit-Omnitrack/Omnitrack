cc3f3b9d25000fd3c7338c4a41242dca7934a13c
Molefe Tlotlo Molefe <tlotlomolefem@gmail.com>
2025-07-22 16:28:09 +0200

    Update Tasks.cs

--- Models/Tasks.cs
+++ Models/Tasks.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,29 +10,20 @@ namespace UserRoles.Models
 
         public string Name { get; set; }
 
-        public string? AssignedTo { get; set; }
+        [ForeignKey("AssignedToUser")] // This links to the navigation property name
+        public string? AssignedToUserId { get; set; } // This is the actual foreign key column
 
-        public string? AssignedToUserId { get; set; } // Make nullable
+        public Users? AssignedToUser { get; set; } // Navigation property to the User
 
-        [ForeignKey("AssignedToUserId")]
-        public Users? AssignedToUser { get; set; }
-        public string? CreatedBy { get; set; }
+        public string? CreatedBy { get; set; } // This currently stores the CreatedBy user's ID string
+
+        [NotMapped] // If you want to store the username directly when saving, consider this, but it's less ideal.
+        public string? CreatedByUserName { get; set; } // To hold the username for display in scenarios where CreatedBy is the ID.
 
 
         public string Details { get; set; }
 
 
         public DateTime DueDate { get; set; }
-
-
-        // public TaskStatus Status { get; set; } // Enum to track task phase
     }
-
-    /*  public enum TaskStatus
-      {
-          Pending,
-          InProgress,
-          Complete,
-          Blocked
-      } */
 }
