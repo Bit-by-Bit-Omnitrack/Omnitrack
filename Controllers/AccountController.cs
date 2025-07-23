a65da503775883e0c98c7ea991f03692b104f09c
Azola.Jokweni <Azola.Jokweni@INSPD5T50TY2>
2025-07-23 12:38:06 +0200

    Update

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -185,14 +185,14 @@ namespace UserRoles.Controllers
                         user.Email,
                         "Welcome to OmniTrack!",
                         $@"
-<div style='text-align:center;'>
-    <img src='https://i.imgur.com/J9Z3ce5.jpeg' alt='OmniTrack Logo' style='max-width:200px; margin-bottom:20px;'/>
-</div>
-<p>Welcome {user.FullName},</p>
-<p>Thank you for registering with <strong>OmniTrack</strong>.</p>
-<p>You have been registered successfully as a <strong>{roleToAssign}</strong>.</p>
-<p>You will be notified once your account is approved.</p>
-<p>Kind regards,<br/>OmniTrack Team</p>"
+                        <div style='text-align:center;'>
+                            <img src='https://i.imgur.com/J9Z3ce5.jpeg' alt='OmniTrack Logo' style='max-width:200px; margin-bottom:20px;'/>
+                        </div>
+                        <p>Welcome {user.FullName},</p>
+                        <p>Thank you for registering with <strong>OmniTrack</strong>.</p>
+                        <p>You have been registered successfully as a <strong>{roleToAssign}</strong>.</p>
+                        <p>You will be notified once your account is approved.</p>
+                        <p>Kind regards,<br/>OmniTrack Team</p>"
                     );
                 }
                 catch (Exception ex)
