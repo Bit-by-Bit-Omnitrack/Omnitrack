a857fc30fa10d758ef7fe8823821c5c02fc1c416
Tshenolo Millicent Mogane <millicent9710@gmail.com>
2025-09-14 19:24:07 +0200

    Add Notification model

---
+++ Models/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace YourProjectNamespace.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }   // The recipient of the notification
+        public string Message { get; set; }
+        public string Type { get; set; }     // Task, Project, Ticket
+        public bool IsRead { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
